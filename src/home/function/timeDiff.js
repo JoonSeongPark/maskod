4b41abb11b23bc3e1079fe93eb6e2e96a5dae007
JoonSeongPark <rytt@yonsei.ac.kr>
2020-05-18 17:46:29 +0900

    time difference calculating func add webpack build

---
+++ src/home/function/timeDiff.js
@@ -0,0 +1,20 @@
+const timeDiff = (time) => {
+  const now = Date.parse(new Date());
+  const date = Date.parse(`${time}`);
+  const timeDiff = (now - date) / 1000;
+  const sec = 60;
+  const min = sec * 60;
+  const hour = min * 24;
+
+  if (timeDiff < sec) {
+    return `${timeDiff}초 전`;
+  } else if (timeDiff < min) {
+    return `${Math.floor(timeDiff / sec)}분 전`;
+  } else if (timeDiff < hour) {
+    return `${Math.floor(timeDiff / min)}시간 전`;
+  } else {
+    return `${Math.floor(timeDiff / hour)}일 전`;
+  }
+};
+
+export default timeDiff;
