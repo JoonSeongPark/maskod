b7377c8844aaccee727260d762e8a9b34fc77aa5
JoonSeongPark <rytt@yonsei.ac.kr>
2020-05-25 23:17:57 +0900

    euclideanDist.js function reshape

--- src/home/function/euclideanDist.js
+++ src/home/function/euclideanDist.js
@@ -1,3 +1,5 @@
-export default function euclideanDist(x1,y1,x2,y2) {
-  return Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2))
-}
+const euclideanDist = (x1, y1, x2, y2) => {
+  return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+};
+
+export default euclideanDist;
